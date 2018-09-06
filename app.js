e511b3ec0f60caf9640fd9a191f5ea618ae5bb1f
Sandra Hooper <sandrahooper@Sandras-MacBook-Pro.local>
2018-09-06 12:31:47 -0400

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-let form = document.getElementById('submitForm');
+//when form is submitted, grab the file data
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    console.log("I was clicked");
-    
-});
-
-
+//set the image block src as the file from the input
+function test() {
+    var image = document.querySelector('#image');
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+  
+    reader.addEventListener("load", function () {
+      image.src = reader.result;
+    }, false);
+  
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  }
