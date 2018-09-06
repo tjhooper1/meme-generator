c152ff367d7f12ad6c2148de0fccf9594ad7fa4e
tjhooper1 <tomh386@gmail.com>
2018-09-06 14:41:26 -0400

    added functionality

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+let topText = document.querySelector('#topText');
+let bottomText = document.querySelector('#bottomText');
+let topMemeText = document.querySelector('.top-text');
+let bottomMemeText = document.querySelector('.bottom-text');
+
 //when form is submitted, grab the file data
 
 //set the image block src as the file from the input
@@ -7,10 +12,28 @@ function test() {
     var reader  = new FileReader();
   
     reader.addEventListener("load", function () {
-      image.src = reader.result;
+        image.src = reader.result; 
     }, false);
   
     if (file) {
       reader.readAsDataURL(file);
     }
   }
+
+  function changeTopText(e){
+      topText.addEventListener('input', e => {
+        console.log(e.target.value);
+      });
+      
+  }
+  function changeBottomText(e){
+      bottomText.addEventListener('input', e => {
+        console.log(e.target.value);
+      });
+      
+  }
+
+  changeTopText();
+  changeBottomText();
+
+  
